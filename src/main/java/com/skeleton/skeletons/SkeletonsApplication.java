2dad4d983244b7602a521f90f50dc84a702ce572
Lucas Dante <lucasd_99@hotmail.com>
2022-09-24 17:28:51 +0200

    Initial commit

---
+++ src/main/java/com/skeleton/skeletons/SkeletonsApplication.java
@@ -0,0 +1,13 @@
+package com.skeleton.skeletons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeletonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkeletonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skeleton/skeletons/SkeletonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skeleton.skeletons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
